5516232cf94c208f38643a010bb849a5ed2dba44
tajansari612 <tajansari612@gmail.com>
2024-12-20 11:57:02 +0530

    new topic added

---
+++ 01_java_basics/_03_variables.java
@@ -0,0 +1,46 @@
+public class _03_variables {
+    public static void main(String[] args) {
+
+        // In Java, variables are containers that hold data, and data types 
+        // define the type of data a variable can store. 
+        // Here’s an overview:
+
+        // Variables in Java
+        // Types of Variables:
+
+        // 01.Local Variables:
+        // Declared inside a method, constructor, or block.
+        // Scope is limited to where they are declared.
+        // Must be initialized before use.
+        
+        // void method() {
+        //     int localVar = 10; // Local variable
+        // }
+
+
+        // 02.Instance Variables:
+        // Declared outside methods but inside a class (non-static).
+        // Each object of the class has its own copy.
+        // Default initialized based on the data type.
+        
+        // class MyClass {
+        //     int instanceVar = 5; // Instance variable
+        // }
+
+
+        // 03.Static Variables:
+        // Declared with the static keyword.
+        // Shared among all instances of the class.
+        // Accessed using the class name.
+        
+        // class MyClass {
+        //     static int staticVar = 20; // Static variable
+        // }
+
+
+        // Rules for Declaring Variables:
+        // Variables must have a valid identifier (name).
+        // Cannot use reserved keywords.
+        // Variable names are case-sensitive.
+    }
+}
