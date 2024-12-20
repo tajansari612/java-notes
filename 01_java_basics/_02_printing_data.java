f341af1a6f47be67aa4e12827b92cb1a94168e55
tajansari612 <tajansari612@gmail.com>
2024-12-20 11:42:38 +0530

    new topic added

---
+++ 01_java_basics/_02_printing_data.java
@@ -0,0 +1,21 @@
+public class _02_printing_data {
+    public static void main(String[] args) {
+        //01 print data in single line
+        System.out.print("Taj");
+        System.out.print(" Mohammad");
+        System.out.print(" Ansari");
+
+        //02 println: to print data in multiple lines, prints every new data
+        //in next line after println
+        System.out.println();
+        System.out.println("Taj");
+        System.out.println("Mohammad");
+        System.out.println("Ansari");
+
+        //03 \n: \n is used in print statement to break the line into next line
+        System.out.println("Taj\nMohammad\nAnsari");
+
+        //Example
+        System.out.println("*\n**\n***\n****");
+    }
+}
