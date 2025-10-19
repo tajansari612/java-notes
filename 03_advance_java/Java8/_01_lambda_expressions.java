940e259ba2c540ce07404a483e8ecb8bfc87751e
2424099 <TajMohammad.Ansari@travelandleisure.com>
2025-10-19 13:10:10 +0530

    Moved to root folder

--- 03_advance_java/Java8/_01_lambda_expressions.java
+++
@@ -1,7 +0,0 @@
-
-public class _01_lambda_expressions {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");  //Basic
-        () -> System.out.println("Hello World!"); //Lambda Expression
-    }
-}
